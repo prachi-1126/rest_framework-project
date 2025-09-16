6f67a26311977bfa0b051831ed27b88b7f6e3659
Prachi Nimbalkar <nimbalkarp222@gmail.com>
2025-09-16 16:09:00 +0530

    create model

--- student_management_backend/students_api/models.py
+++ student_management_backend/students_api/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Student(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    student_id = models.CharField(max_length=50, unique=True)
+    date_of_birth = models.DateField(null=True, blank=True)
+    contact_email = models.EmailField(unique=True)
+    phone_contact = models.CharField(max_length=20, blank=True, null=True)
+    parent_contact_phone = models.CharField(max_length=20, blank=True, null=True)
+    
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
