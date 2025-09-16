3b08709e3f553b6fdc164f5f613ced819efeded5
Prachi Nimbalkar <nimbalkarp222@gmail.com>
2025-09-17 01:28:11 +0530

    updated views and urls

--- student_management_backend/students_api/views.py
+++ student_management_backend/students_api/views.py
@@ -1,15 +1,82 @@
-# students_api/views.py
-
-from rest_framework import viewsets
-from rest_framework.permissions import IsAuthenticated
+from rest_framework import viewsets, generics, status
+from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework_simplejwt.authentication import JWTAuthentication
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
+from django.contrib.auth.models import User
+from django.contrib.auth.tokens import PasswordResetTokenGenerator
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
+from django.utils.encoding import force_bytes, force_str
+from django.urls import reverse
+from django.core.mail import send_mail
+from django.conf import settings
 
 from .models import Student
 from .serializers import StudentSerializer
+# from .serializers import UserSerializer # You may need to create this for the User model
 
+
+# Your existing StudentViewSet
 class StudentViewSet(viewsets.ModelViewSet):
     queryset = Student.objects.all()
     serializer_class = StudentSerializer
-    
     authentication_classes = [JWTAuthentication]
     permission_classes = [IsAuthenticated]
+
+
+# The view to handle password reset requests
+class PasswordResetRequestView(APIView):
+    permission_classes = [AllowAny]
+
+    def post(self, request, *args, **kwargs):
+        email = request.data.get('email')
+        if not email:
+            return Response({'error': 'Email is required'}, status=status.HTTP_400_BAD_REQUEST)
+
+        try:
+            user = User.objects.get(email=email)
+        except User.DoesNotExist:
+            return Response({'success': 'Password reset email sent if user exists'}, status=status.HTTP_200_OK)
+
+        token_generator = PasswordResetTokenGenerator()
+        uid = urlsafe_base64_encode(force_bytes(user.pk))
+        token = token_generator.make_token(user)
+
+        # The reset URL will be on your FRONT-END
+        reset_url = reverse('password_reset_confirm', kwargs={'uidb64': uid, 'token': token})
+        reset_url_full = f"http://127.0.0.1:8000{reset_url}" # CHANGE THIS TO YOUR FRONT-END URL
+
+        send_mail(
+            'Password Reset Request',
+            f'Click the link to reset your password: {reset_url_full}',
+            settings.EMAIL_HOST_USER,
+            [user.email],
+            fail_silently=False,
+        )
+
+        return Response({'success': 'Password reset email sent if user exists'}, status=status.HTTP_200_OK)
+
+# The view to confirm the password reset
+class PasswordResetConfirmView(APIView):
+    permission_classes = [AllowAny]
+
+    def post(self, request, *args, **kwargs):
+        uidb64 = request.data.get('uidb64')
+        token = request.data.get('token')
+        new_password = request.data.get('new_password')
+        
+        try:
+            uid = force_str(urlsafe_base64_decode(uidb64))
+            user = User.objects.get(pk=uid)
+        except (TypeError, ValueError, OverflowError, User.DoesNotExist):
+            return Response({'error': 'Invalid token or user ID'}, status=status.HTTP_400_BAD_REQUEST)
+
+        token_generator = PasswordResetTokenGenerator()
+        if not token_generator.check_token(user, token):
+            return Response({'error': 'Invalid token or user ID'}, status=status.HTTP_400_BAD_REQUEST)
+        
+        user.set_password(new_password)
+        user.save()
+
+        return Response({'success': 'Password reset successful'}, status=status.HTTP_200_OK)
